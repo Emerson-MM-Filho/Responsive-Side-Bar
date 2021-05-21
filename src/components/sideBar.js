9ff1065407d26552f29a5a65eab7d9c1e111916d
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-21 15:23:36 -0400

    create button in accountArea

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -3,10 +3,11 @@ import Button from "./asideButton";
 
 import { FaReact } from 'react-icons/fa';
 import { RiHomeLine } from 'react-icons/ri'
-import { BsListUl, BsPeople, BsGear } from 'react-icons/bs'
+import { BsListUl, BsPeople, BsGear, BsPerson } from 'react-icons/bs'
 import { BiMessageRoundedDetail } from 'react-icons/bi'
 import { HiOutlineFolderOpen } from 'react-icons/hi'
 import { IoMdNotificationsOutline } from 'react-icons/io'
+import { CgLogOut } from 'react-icons/cg'
 
 function sideBar() {
   return (
@@ -27,7 +28,8 @@ function sideBar() {
         <Button icon={<IoMdNotificationsOutline/>} text='Notifications'/>
       </div>
       <div id="accountArea">
-
+        <Button icon={<BsPerson/>} text='Account'/>
+        <Button icon={<CgLogOut/>} text='Logout'/>
       </div>
     </aside>
   )
