a722957ea0ed73d28858f620fd6344809a239341
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-22 12:46:53 -0400

    stylizes buttons hover size and color

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -15,24 +15,26 @@ const iconSize = '1.2rem'
 function sideBar() {
   return (
     <aside>
-      <div id="logoArea">
-        <FaReact size={"4rem"}/>
-        <h1>Company</h1>
-      </div>
-      <div id="menuArea">
-        <Button icon={<RiHomeLine size={iconSize}/>} text='Dashboard'/>
-        <Button icon={<IoList size={iconSize}/>} text='Projects'/>
-        <Button icon={<BiMessageRoundedDetail size={iconSize}/>} text='Messages'/>
-        <Button icon={<HiOutlineFolderOpen size={iconSize}/>} text='Services'/>
-        <Button icon={<BsPeople size={iconSize}/>} text='Customers'/>
-      </div>
-      <div id="configsArea">
-        <Button icon={<BsGear size={iconSize}/>} text='Settings'/>
-        <Button icon={<IoMdNotificationsOutline size={iconSize}/>} text='Notifications'/>
-      </div>
-      <div id="accountArea">
-        <Button icon={<BsPerson size={iconSize}/>} text='Account'/>
-        <Button icon={<CgLogOut size={iconSize}/>} text='Logout'/>
+      <div id="menuContainer">
+        <div id="logoArea">
+          <FaReact id="logoIcon" size={"4rem"}/>
+          <h1 id="companyName">Company</h1>
+        </div>
+        <div id="menuArea">
+          <Button icon={<RiHomeLine size={iconSize}/>} text='Dashboard'/>
+          <Button icon={<IoList size={iconSize}/>} text='Projects'/>
+          <Button icon={<BiMessageRoundedDetail size={iconSize}/>} text='Messages'/>
+          <Button icon={<HiOutlineFolderOpen size={iconSize}/>} text='Services'/>
+          <Button icon={<BsPeople size={iconSize}/>} text='Customers'/>
+        </div>
+        <div id="configsArea">
+          <Button icon={<BsGear size={iconSize}/>} text='Settings'/>
+          <Button icon={<IoMdNotificationsOutline size={iconSize}/>} text='Notifications'/>
+        </div>
+        <div id="accountArea">
+          <Button icon={<BsPerson size={iconSize}/>} text='Account'/>
+          <Button icon={<CgLogOut size={iconSize}/>} text='Logout'/>
+        </div>
       </div>
     </aside>
   )
